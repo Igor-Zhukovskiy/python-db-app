3eff04dd4fc928b6ba6a9070903b253d4f74ecd9
Laptop 3 <obivan-kenobo@yandex.ru>
2022-06-11 12:02:11 +0300

    main-4

--- index.py
+++ index.py
@@ -2,19 +2,25 @@
 
 # это файл для старта программы
 
+# ипортирование модулей python
 from asyncore import ExitNow
 import tkinter as tk
 from tkinter import ttk
+import mysql.connector as mysql
+
+# импротирование локальных модулей
 from modules.login import Login
 
 # основной класс программы
 class Main(tk.Frame):
+    # конструктор
     def __init__(self, root):
         self.root = root
         # позволяет реализовывать функцию start(), без него работать не будет
         super().__init__(root)
         self.initMain()
-
+    
+    # метод для отрисовки основного окна
     def initMain(self):
         toolBar = tk.Frame(bg='#aaaaaa', bd=5)
         toolBar.pack(side=tk.TOP, fill=tk.X)
@@ -27,13 +33,18 @@ class Main(tk.Frame):
 
         btnSearch = tk.Button(toolBar, text="Search", bd=0)
         btnSearch.pack(side=tk.LEFT)
+    
+    def getData(self):
+        dbData = mysql.connect
 
+# функция запуску окна входа пользователя
+# использует условие с exit(), если не выполняется то остонавливает выполнения программы 
 def loginCheck():
     login = Login()
     login.root.mainloop()
     if login.loginStatus == False:
         exit()
-
+# функция запуска основного приложения
 def start():
     root = tk.Tk()
     mainWindow = Main(root)
@@ -42,7 +53,6 @@ def start():
     root.geometry('650x450+300+200')
     root.mainloop()
 
-# выполняет функцию start() если имя обьекта является "__main__"
 if __name__ == "__main__":
     loginCheck()
     start()
